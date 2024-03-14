98362c7b6fec70545eedefeae85bfe4f48aebf62
HodeX7 <mehtaakshar1234@gmail.com>
2024-03-15 01:48:10 +0530

    build: Initialised the Project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "./ui/Button";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between py-5 px-16">
+      <div>
+        <h1>Courier Service</h1>
+      </div>
+      <div className="flex gap-8 items-center">
+        <h1>About Us</h1>
+        <h1>Contact Us</h1>
+        <Button text="Log In" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ text }) => {
+  return <button className="bg-yellow-500 px-5 py-2 rounded-md">{text}</button>;
+};
+
+export default Button;
