4e6be678a2c69e7e8d915bf8b058f1411d7488a2
DhruvLohar <dhruvlohar09@gmail.com>
2024-03-23 21:55:43 +0530

    added landing page content

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,92 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import Button from "@/components/ui/Button";
 
+const howItWorksData = [
+  {
+    image: "howItWorks_01.png",
+    title: "Insert Courier 📫",
+    content: "Simply place your courier in one of our designated mailbooths located in partnered grocery stores.",
+  },
+  {
+    image: "howItWorks_02.png",
+    title: "Select Delivery Speed 📦",
+    content: "Choose your preferred delivery speed from our available options displayed on the screen.",
+  },
+  {
+    image: "howItWorks_03.png",
+    title: "Relax and Track 😁",
+    content: "Sit back and relax as our partnered courier companies, including Indian Post, Blue Dart, Porter, and Gajanan Courier Services, ensure timely delivery to your desired destination.",
+  }
+];
+
+const whyUsData = [
+  {
+    image: "howItWorks_01.png",
+    title: "Convenience.",
+    content: "Our mailbooths are conveniently located within grocery stores, making it effortless for you to drop off your courier while running errands.",
+  },
+  {
+    image: "howItWorks_02.png",
+    title: "Reliability",
+    content: "Partnering with trusted courier companies guarantees reliable and secure delivery of your parcels.",
+  },
+  {
+    image: "howItWorks_03.png",
+    title: "Speed Options",
+    content: "Select from a range of delivery speeds to suit your urgency, ensuring your courier reaches its destination on time.",
+  },
+  {
+    image: "howItWorks_03.png",
+    title: "Transparency",
+    content: "Track your courier every step of the way, knowing exactly when it's picked up and delivered.",
+  }
+];
+
+const partnersData = [
+  {
+    logo: "/india_post.jpg",
+    name: "Indian Post",
+    info: "20 km radius",
+    content: "Indian Post is a reliable postal service provider known for its widespread network and affordable rates. With Mailbooth, you can enjoy the convenience of Indian Post's services within a 20 km radius, delivering your parcels safely and efficiently."
+  },
+  {
+    name: "Blue Dart",
+    info: "40 km radius",
+    content: "Blue Dart is a leading logistics company renowned for its express delivery services and nationwide coverage. Through our partnership with Blue Dart, we offer expedited shipping options within a 40 km radius, ensuring swift delivery of your parcels to their destination."
+  },
+  {
+    name: "Porter",
+    info: "150 km radius",
+    content: "Porter specializes in rapid courier services, catering to time-sensitive deliveries within a 150 km radius. With Mailbooth, you can leverage Porter's efficient delivery network to send your parcels with speed and precision."
+  },
+  {
+    name: "Gajanan Courier Services",
+    info: "30 km radius",
+    content: "Gajanan Courier Services is known for its reliable and cost-effective courier solutions, serving customers within a 30 km radius. With Mailbooth, you can trust Gajanan Courier Services to deliver your parcels promptly and securely to their intended recipients."
+  }
+];
+
+
 export default function Home() {
-  const services = [
-    {
-      image: "",
-      title: "Swift Delivery",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      image: "",
-      title: "Swift Delivery",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      image: "",
-      title: "Swift Delivery",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      image: "",
-      title: "Swift Delivery",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-  ];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       {/* Hero Section */}
       <div className="w-full flex items-center justify-center h-screen bg-background text-white">
         <div className="w-1/2 flex flex-col gap-y-5">
-          <h1 className="text-6xl font-black">
-            Reliable Service <br /> Every Time
+          <h1 className="text-5xl font-black">
+            Welcome to MailBooth - <br /> <span className="text-4xl font-semibold">Simplifying Courier Services !</span>
           </h1>
-          <p className="w-[70%]">
-            Nullam ac aliquam purus. Donec tempor, metus sed porttitor posuere,
-            elit sapien rutrum elit, eget tincidunt nisl tortor nec metus. Donec
-            tempor rhoncus convallis.
+          <p className="w-[75%] text-gray-400 text-justify">
+            At Mailbooth, we're revolutionizing the way you send and receive couriers. Our mission is to provide convenient, reliable, and efficient courier services by partnering with grocery stores and courier companies to install physical mailbooths in strategic locations across India and Canada.
           </p>
-          <div className="flex gap-x-2 mt-6 w-2/3">
-            <input
-              placeholder="Enter Delivery Address"
-              className="rounded-md p-2 flex-1"
-            />
-            <Button text="Go" />
-          </div>
+          <Link href={"/form/senderDetails"}>
+            <div className="flex gap-x-2 mt-6 w-2/3">
+              <Button text="Get Started!" />
+            </div>
+          </Link>
         </div>
         <iframe
           className="pl-16"
@@ -54,34 +97,32 @@ export default function Home() {
       </div>
 
       {/* 2nd Section */}
-      <div className="w-full flex py-20 px-24">
-        <div className="w-1/2">Image</div>
+      <div className="w-full flex items-center justify-center py-12 px-24">
+        <div className="w-1/2">
+          <Image src={"/our_mission.gif"} width={400} height={400} />
+        </div>
         <div className="w-1/2 flex flex-col gap-y-5">
-          <h1 className="text-4xl font-extrabold">
-            Do You Want A Fast <br /> Service? Just Call Us.
+          <h1 className="text-4xl font-extrabold text-white">
+            Our  Mission.
           </h1>
           <h4 className="text-gray-400 text-md">
-            Aenean quis sagittis sem. Sed volutpat quam a imperdiet volutpat.
-            Quisque maximus nibh elit, nec molestie erat tincidunt sit amet.
-            Duis nec ante molestie, volutpat mi ac, convallis quam. Fusce
-            laoreet bibendum luctus. Maecenas malesuada fermentum mi.
+            At Mailbooth, our mission is to make courier services more accessible and hassle-free for everyone. By partnering with grocery stores across India and Canada, we're bringing our innovative mail booths right to your neighborhood, making it easier than ever to send your parcels securely and swiftly.
           </h4>
-          <Button text="Call Us" classes={"w-[40%] mt-6"} />
+          <Button text="Explore More" classes={"w-[40%] mt-6"} />
         </div>
       </div>
 
       {/* 3rd Section */}
       <div className="w-full h-screen flex flex-col items-center justify-center bg-background relative py-20 px-24">
         <h1 className="text-4xl font-extrabold text-center text-white">
-          Try Us And See How Good <br />
-          Our Services Are.
+          How It Works ?
         </h1>
-        <Button text="Call Us" classes={"w-32 mt-6"} />
-        <div className="flex items-center justify-center space-x-8 mt-auto z-[2] px-20">
-          {services.map((item, idx) => (
-            <div key={idx} className="flex flex-col items-center gap-y-4 x-6">
+        <Button text="Get More Details" classes={"w-64 mt-6"} />
+        <div className="flex items-center justify-center space-x-8 mt-auto z-[2]">
+          {howItWorksData.map((item, idx) => (
+            <div key={idx} className="w-[20rem] flex flex-col items-center gap-y-4">
               <div className="w-42 h-42 bg-gray flex items-center justify-center">
-                image
+                <Image src={`/${item.image}`} width={120} height={120} />
               </div>
               <h2 className="text-2xl font-medium">{item.title}</h2>
               <p className="text-md text-center">{item.content}</p>
@@ -92,46 +133,23 @@ export default function Home() {
       </div>
 
       {/* 4th Section */}
-      <div className="w-full h-screen flex flex-col items-center justify-center bg-white py-20 px-24">
-        <h1 className="text-4xl font-extrabold text-center mt-4">
-          We Deliver Everything
-        </h1>
-        <p className="w-[35%] text-center mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-          tellus, luctus nec ullamcorper.
-        </p>
-        <div className="flex items-center justify-center space-x-8 mt-auto z-2 px-20">
-          {services.slice(0, 3).map((item, idx) => (
-            <div key={idx} className="flex flex-col items-center gap-y-4 x-6">
-              <div className="w-42 h-42 bg-gray flex items-center justify-center">
-                image
-              </div>
-              <h2 className="text-2xl font-medium">{item.title}</h2>
-              <p className="text-md text-center">{item.content}</p>
-              <Button text="Order Now" classes={"w-32 mt-4"} />
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* 5th Section */}
       <div
-        className="w-full h-screen flex flex-col items-center justify-center bg-white relative text-white"
+        className="w-full min-h-screen flex flex-col items-center justify-center bg-white relative text-white"
         style={{
           backgroundImage:
-            "url(https://kitpro.site/delivey/wp-content/uploads/sites/70/2022/02/young-asian-male-courier-with-thermo-bag-wearing-f-2021-09-03-20-53-03-utc-copy.jpg)",
+            "url(https://kitpro.site/delivey/wp-content/uploads/sites/70/2022/02/young-asian-male-courier-with-thermo-bag-wearing-f-2021-09-03-20-53-03-utc-copy.jpg) center no-repeat cover",
         }}
       >
         <div className="tint absolute top-0 left-0 w-full h-full bg-background opacity-80"></div>
-        <div className="z-[2] w-full h-full flex flex-col items-center justify-center py-20">
-          <h1 className="text-4xl font-extrabold text-center mt-12">
-            Join The Crew
+        <div className="z-[2] w-full h-full flex flex-col items-center justify-center py-12">
+          <h1 className="text-4xl font-extrabold text-center my-12">
+            Why Choose MailBooth ?
           </h1>
-          <div className="flex items-center justify-center space-x-8 z-2 px-20 mt-auto">
-            {services.slice(0, 3).map((item, idx) => (
+          <div className="flex items-center justify-center space-x-8 z-2 px-20 mt-10">
+            {whyUsData.slice(0, 2).map((item, idx) => (
               <div
                 key={idx}
-                className="flex flex-col items-start relative gap-y-4 x-6 bg-background px-[2rem] py-[3rem] max-w-[22rem]"
+                className="flex flex-col items-start relative gap-y-4 x-6 bg-background px-[2rem] py-[3rem] w-[45%] h-[14rem]"
               >
                 <div
                   className="w-[6rem] h-[6rem] absolute bg-gray flex items-center justify-center bg-red-500"
@@ -141,7 +159,23 @@ export default function Home() {
                 </div>
                 <h2 className="text-3xl font-medium mt-4">{item.title}</h2>
                 <p className="text-md">{item.content}</p>
-                <Button text="Order Now" classes={"w-32 mt-4"} />
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center justify-center space-x-8 z-2 px-20 mt-20">
+            {whyUsData.slice(2, 4).map((item, idx) => (
+              <div
+                key={idx}
+                className="flex flex-col items-start relative gap-y-4 x-6 bg-background px-[2rem] py-[3rem] w-[45%] h-[14rem]"
+              >
+                <div
+                  className="w-[6rem] h-[6rem] absolute bg-gray flex items-center justify-center bg-red-500"
+                  style={{ top: "-3rem" }}
+                >
+                  image
+                </div>
+                <h2 className="text-3xl font-medium mt-4">{item.title}</h2>
+                <p className="text-md">{item.content}</p>
               </div>
             ))}
           </div>
@@ -149,50 +183,25 @@ export default function Home() {
       </div>
 
       {/* 6th Section */}
-      <div className="w-full h-screen flex flex-col items-center justify-center bg-white relative py-20">
+      <div className="w-full h-screen flex flex-col items-center justify-center bg-white relative py-20 overflow-x-none">
         <div className="curve absolute top-0 bg-background w-[115%] h-[60vh] rounded-b-full"></div>
         <div className="z-[2] w-full flex flex-col items-center">
           <h1 className="text-4xl font-extrabold text-center text-white">
-            Customer Testimonials
+            Our Partnerships
           </h1>
-          <Button text="Get More" classes={"w-32 mt-4"} />
+          <Button text="Learn More ..." classes={"w-64 mt-4"} />
         </div>
-        <div className="flex items-center justify-center space-x-8 mt-12 z-[2] px-20">
-          <div className="flex flex-col items-center px-6 bg-white rounded-xl py-4">
-            <div className="w-[6rem] h-[6rem] bg-gray flex items-center justify-center rounded-full bg-red-500">
-              image
-            </div>
-            <h2 className="text-xl font-medium mt-4 mb-1">Emily Rose</h2>
-            <h2 className="text-md font-medium">Teacher</h2>
-            <div className="stars flex items-center justify-center space-x-2 my-4">
-              {Array.from({ length: 5 }).map((_, i) => (
-                <svg
-                  key={i}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="#FF8A65"
-                >
-                  <path
-                    d="m13.73 3.51 1.76 3.52c.24.49.88.96 1.42 1.05l3.19.53c2.04.34 2.52 1.82 1.05 3.28l-2.48 2.48c-.42.42-.65 1.23-.52 1.81l.71 3.07c.56 2.43-.73 3.37-2.88 2.1l-2.99-1.77c-.54-.32-1.43-.32-1.98 0l-2.99 1.77c-2.14 1.27-3.44.32-2.88-2.1l.71-3.07c.13-.58-.1-1.39-.52-1.81l-2.48-2.48c-1.46-1.46-.99-2.94 1.05-3.28l3.19-.53c.53-.09 1.17-.56 1.41-1.05l1.76-3.52c.96-1.91 2.52-1.91 3.47 0Z"
-                    stroke="#FF8A65"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-              ))}
-            </div>
-            <p className="text-md text-center">
-              Sed sollicitudin magna sit amet lacinia consectetur. Vivamus
-              rhoncus accumsan facilisis. Praesent mi mi, placerat eu metus ac,
-              porttitor laoreet sem. Praesent sed sem sed augue semper iaculis
-              eu in eros. Morbi sed enim at risus semper placerat. Ut non enim
-              vitae elit sagittis bibendum venenatis sed nunc.
+        <div className="flex items-center justify-center space-x-8 mt-12 z-[2] px-20 overflow-x">
+          <div className="flex flex-col items-center px-6 bg-white rounded-xl py-4 border border-background">
+            <Image src={partnersData[0].logo} width={300} height={300} className="rounded-full object-cover" />
+            <h2 className="text-xl font-medium mt-4 mb-1">{partnersData[0].name}</h2>
+            <h2 className="text-gray-500 text-md font-medium">{partnersData[0].info}</h2>
+            <p className="text-md text-center mt-4">
+              {partnersData[0].content}
             </p>
           </div>
         </div>
+
       </div>
     </main>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,40 +3,26 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="bg-background p-20 flex flex-col items-start text-white">
-      <h1 className="text-2xl font-black mb-8">Mail Box</h1>
+      <h1 className="text-2xl font-black mb-8">Mail Booth</h1>
       <div className="flex justify-center items-start space-x-[6rem]">
-        <div className="footer-item space-y-2">
-          <h2 className="font-semibold text-xl mb-2">Company</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
+        <div className="flex flex-col footer-item space-y-2">
+          <h2 className="font-semibold text-xl mb-2 w-[10rem]">Links</h2>
+          <a href="#" className="text-sm">Home</a>
+          <a href="#" className="text-sm">Our Mission</a>
+          <a href="#" className="text-sm">How It Works</a>
+          <a href="#" className="text-sm">Why Choose Us?</a>
+          <a href="#" className="text-sm">Our Partnerships</a>
         </div>
         <div className="footer-item space-y-2">
-          <h2 className="font-semibold text-xl mb-2">Company</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
+          <h2 className="font-semibold text-xl mb-2">Find Us!</h2>
+          <p className="text-sm">Explore our growing network of mailbooth locations across India and Canada. Easily locate the nearest mailbooth in your city or neighborhood.</p>
         </div>
         <div className="footer-item space-y-2">
-          <h2 className="font-semibold text-xl mb-3">Company</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-        </div>
-        <div className="footer-item space-y-2">
-          <h2 className="font-semibold text-xl mb-2">Company</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-        </div>
-        <div className="footer-item space-y-2">
-          <h2 className="font-semibold text-xl mb-2">Company</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
-          <h2 className="text-sm">About</h2>
+          <h2 className="font-semibold text-xl mb-2">Have Questions? Contact Us</h2>
+          <p className="text-sm">Have inquiries, feedback, or interested in partnership opportunities? Reach out to us via our <a href="#" className="underline font-bold cursor-pointer">inquiry form</a>, email, phone, or visit us at our physical address. We're here to assist you every step of the way.</p>
         </div>
       </div>
-      <span className="text-center mt-8">
+      <span className="text-gray-400 text-sm text-center mt-8">
         Copyright © 2024 | Developed by{" "}
         <Link href={"https://www.computeitsolutions.com"}>Devvebs</Link>
       </span>
